437a69650cf69c2f2eb9e593684d292e425d3e5f
mizzo <samuelhope805@gmail.com>
2024-06-28 18:07:45 +0300

    Updated server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -6,30 +8,33 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let users = [
-  { id: 1, name: 'User 1' },
-  { id: 2, name: 'User 2' },
-  { id: 3, name: 'User 3' },
-  { id: 4, name: 'User 4' },
-  { id: 5, name: 'User 5' }
+// Dummy users data
+const users = [
+    { id: '1', name: 'User 1' },
+    { id: '2', name: 'User 2' },
+    { id: '3', name: 'User 3' },
+    { id: '4', name: 'User 4' },
+    { id: '5', name: 'User 5' }
 ];
 
-io.on('connection', socket => {
-  console.log('A user connected');
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
 
-  socket.emit('users', users); // Send list of users to the client
+    // Emit list of users when a client connects
+    socket.emit('users', users);
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    // Handle incoming messages
+    socket.on('message', (data) => {
+        console.log('Message received:', data);
+        io.emit('message', data); // Broadcast message to all clients
+    });
 
-  socket.on('chat message', ({ sender, receiver, message }) => {
-    console.log(`Message from ${sender} to ${receiver}: ${message}`);
-    io.emit('chat message', { sender, receiver, message });
-  });
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
